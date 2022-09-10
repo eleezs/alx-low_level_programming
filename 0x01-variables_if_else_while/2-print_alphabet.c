2864453fd641faa2ccd957198e5d917eb0dc9319
eleezs <wilduche@gmail.com>
2022-09-10 05:49:16 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char c = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(c);
+
+	return (0);
+}
