9d8c34c6b7ee30f27a13abd32ba972b662b32b50
eleezs <wilduche@gmail.com>
2022-09-11 05:42:34 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c[100] = "0123456789abcdef";
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
