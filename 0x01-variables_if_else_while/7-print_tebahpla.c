edeb573409a83ffd4d9fe6d79c33115d325b1d1a
eleezs <wilduche@gmail.com>
2022-09-10 23:06:10 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[100] = "abcdefghijklmnopqrstuvwxyz";
+
+	int leng;
+
+	int g;
+
+	leng = strlen(c);
+
+	for (g = leng - 1; g >= 0; g--)
+	{
+		putchar(c[g]);
+	}
+
+	putchar('\n');
+	return (0);
+}
