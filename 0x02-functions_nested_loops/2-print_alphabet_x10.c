87062c2065dcc8c0a4a9bb1f75aee20a6d003bab
eleezs <wilduche@gmail.com>
2022-09-13 15:03:02 -0700

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Retuen: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int N;
+
+	char c;
+	
+	while (N <= 10)
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+		{
+			_putchar(c);
+		}
+		N++;
+	}
+	_printchar('\n');
+}
