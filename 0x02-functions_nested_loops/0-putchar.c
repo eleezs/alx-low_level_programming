65da499726ead6c70ec5a30e65343f1fd6d8fc89
eleezs <wilduche@gmail.com>
2022-09-13 14:35:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
+/**
  * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
