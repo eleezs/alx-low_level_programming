d158849e8edaaa1e08435a9bd1ab39a04ba40ab9
eleezs <wilduche@gmail.com>
2022-09-13 11:53:02 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,12 @@
-#include <stdio.h>
 #include "main.h"
-void print_alphabet(void);
 
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always0.
  */
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	for (c = 'a'; c <= 'z'; ++c)
-	{
-		putchar(c);
-	}
-	putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,13 @@
 #include <stdio.h>
 void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c ='a'; c <= 'z'; ++c)
+	{
+		putcher(c);
+	}
+	putchar('\n');
+}
