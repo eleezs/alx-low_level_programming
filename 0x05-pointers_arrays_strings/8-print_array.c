1a6abed967cd4ce15f4292745aa3239b33f71550
eleezs <wilduche@gmail.com>
2022-09-20 06:10:19 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - main function
+ * @a: argument
+ * @n: argument
+ * Return: always
+ */
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	for (b = 0; b <= n; b++)
+	{
+		printf("%d\n", a[b]);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,8 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
+void _puts(char *str);
+
+void print_array(int *a, int n);
+
 #endif
