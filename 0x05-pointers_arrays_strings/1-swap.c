60fd1e07ffe4b600a9f82945b91ee5aa99943a04
eleezs <wilduche@gmail.com>
2022-09-19 19:40:05 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - main function
+ * @a: an argument
+ * @b: an argument
+ * Return: Always 0 (success)
+ */
+swap_int(int *a, int *b)
+{
+	
+}
